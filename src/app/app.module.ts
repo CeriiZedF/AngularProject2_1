df3889910e16a0379156161c9824cdca42a927a9
CeriiZedF <dasbalan14@gmail.com>
2024-04-01 20:55:35 +0200

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProviderComponent } from './provider/provider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProviderComponent
   ],
   imports: [
     BrowserModule,
